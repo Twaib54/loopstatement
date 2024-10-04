fdc09df772533d346bf88034067da204a84c9384
Twaib54 <twaibtwizzy506@gmail.com>
2024-10-04 12:24:35 +0300

    Add files via upload

---
+++ Loopingstatement.java
@@ -0,0 +1,13 @@
+package loopingstatements;
+
+public class Loopingstatement {
+
+	public static void main(String[] args) {
+		//Loop to print prime numbers 9 to 21
+		for (int x = 9; x <= 21; x+= 2) {
+			System.out.println(x);
+		}
+
+	}
+
+}
